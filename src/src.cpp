21421fbe9fc9f0139393a39607554122ca3c7341
Pavel Avramenko <androidpasha@gmail.com>
2024-02-07 23:42:25 +0200

    v.1b

--- src/src.cpp
+++ src/src.cpp
@@ -2,32 +2,67 @@
 #include "QuickShiftArray.h"
 #include <array>
 #include <vector>
-#define ARR_SIZE 50            //Количество ячеек массива
+#define ARR_SIZE 100            //Количество ячеек массива
 #define ITERATIONS 500000       // Количество повторений при замере времени
+
+    QuickShiftArray<int> quickShiftArray(ARR_SIZE);
+    std::array<int, ARR_SIZE> stdArr;
+    std::vector<int> vector(ARR_SIZE);
+    int classicArr[ARR_SIZE];
+
+    uint32_t startTime;
+    int c;
+
+template <typename T>
+inline uint32_t forLoop(T &arr){
+  ESP.wdtFeed();
+  int c = ITERATIONS;
+  uint32_t startTime = millis();
+  while (c--)
+    for(int i = 0; i < ARR_SIZE; i++)
+      arr[i] = 0;
+  return millis()-startTime;
+}
+
+template <typename T>
+inline uint32_t rangeBasedForLoop(T &arr){
+  ESP.wdtFeed();
+  int c = ITERATIONS;
+  uint32_t startTime = millis();
+  while (c--)
+    for(auto &e : arr)
+      e = 0;
+  return millis()-startTime;
+}
+
+template <typename T>
+inline uint32_t iteratorForLoop(T &arr){
+  ESP.wdtFeed();
+  int c = ITERATIONS;
+  uint32_t startTime = millis();
+  while (c--)
+      for (auto it =  arr.begin(); it != arr.end(); ++it)
+        *it = 0;
+  return millis()-startTime;
+}
+
 
 void setup()
 {
   ESP.wdtDisable();
   Serial.begin(74880);
-  QuickShiftArray<int> quickShiftArray(ARR_SIZE);
-  std::array<int, ARR_SIZE> stdArr = {0};
-  std::vector<int> vector(ARR_SIZE, 0);
-  volatile int classicArr[ARR_SIZE];
-  uint32_t startTime;
-  int c;
-Serial.printf("\nРазмер quickShiftArray %d, stdArr %d, vector %d", sizeof(quickShiftArray), sizeof(stdArr),sizeof(vector));
 
+
+  Serial.printf("\nРазмер quickShiftArray %d, stdArr %d, vector %d", sizeof(quickShiftArray), sizeof(stdArr),sizeof(vector));
   Serial.printf("\nРазмер масивов по %d ячеек;\nКоличество повторений при замере времени %d раз.\n\n", ARR_SIZE, ITERATIONS);
 
-
+Serial.println("\nВремя сдвига:\n");
 // ____________________Сдвиг QuickShiftArray________________________
   c = ITERATIONS;
   startTime = millis();
   while (c--)
-  {
          quickShiftArray << 1;
-  }
-  Serial.printf("Время сдвига QuickShiftArray: \t%lu мс\n", millis() - startTime);
+  Serial.printf("quickShiftArray \t%lu мс\n", millis() - startTime);
 
 // ____________________Сдвиг класического массива________________________
   c = ITERATIONS;
@@ -39,92 +74,43 @@ Serial.printf("\nРазмер quickShiftArray %d, stdArr %d, vector %d", sizeof(
         classicArr[i] = classicArr[i+1];
     classicArr[ARR_SIZE-1] = first;
   }
-  Serial.printf("Время сдвига classicArr: \t%lu мс\n", millis() - startTime);
+  Serial.printf("classicArr \t\t%lu мс\n", millis() - startTime);
 
 // ____________________ Сдвиг std::arr__________________________
-  c = ITERATIONS; // количество замеров
+  c = ITERATIONS;
   startTime = millis();
   while (c--)
-  {
-    std::rotate(stdArr.begin(), stdArr.begin() + 1, stdArr.end());
-  }
-  Serial.printf("Время сдвига std::array: \t%lu мс\n", millis() - startTime);
+      std::rotate(stdArr.begin(), stdArr.begin() + 1, stdArr.end());
+  Serial.printf("std::array: \t\t%lu мс\n", millis() - startTime);
   ESP.wdtFeed();
   // ____________________ Сдвиг vector__________________________
   c = ITERATIONS; // количество замеров
   startTime = millis();
   while (c--)
-  {
-    std::rotate(vector.begin(), vector.begin() + 1, vector.end());
-  }
-  Serial.printf("Время сдвига std::vector: \t%lu мс\n", millis() - startTime);
-  ESP.wdtFeed();
-// ____________________Заполнение std::arr__________________________
-  c = ITERATIONS; // количество замеров
-  startTime = millis();
-  while (c--)
-  {
-      for (int i = 0; i < ARR_SIZE; i++)
-        stdArr[i] = i;
-  }
-  Serial.printf("Время заполнения std::array: \t%lu мс\n", millis() - startTime);
-  ESP.wdtFeed();
-  // ____________________Заполнение std::vector__________________________
-  c = ITERATIONS; // количество замеров
-  startTime = millis();
-  while (c--)
-  {
-      for (int i = 0; i < ARR_SIZE; i++)
-        vector[i] = i;
-  }
-  Serial.printf("Время заполнения std::vector: \t%lu мс\n", millis() - startTime);
-  ESP.wdtFeed();
- // ____________________Заполнение класического массива________________________
-  c = ITERATIONS; // количество замеров
-  startTime = millis();
-  while (c--)
-  {
-      for (int i = 0; i < ARR_SIZE; i++)
-        classicArr[i] = i;
-  }
-  Serial.printf("Время заполнения classicArr: \t%lu мс\n", millis() - startTime);
-  ESP.wdtFeed();
-// ____________________Заполнение QuickShiftArray________________________
-  c = ITERATIONS;
-  startTime = millis();
-  while (c--)
-  {
-      for (int i = 0; i < ARR_SIZE; i++)
-        quickShiftArray[i] = i;
-  }
-  Serial.printf("Время за-ния QuickShiftArray: \t%lu мс\n", millis() - startTime);
+      std::rotate(vector.begin(), vector.begin() + 1, vector.end());
+  Serial.printf("std::vector: \t\t%lu мс\n", millis() - startTime);
   ESP.wdtFeed();
 
-  // ____________________Заполнение QuickShiftArray циклом по диапазону________________________
-  c = ITERATIONS;
-  startTime = millis();
-  while (c--)
-  {
-      for (auto &e : quickShiftArray)
-          e = 0;
-  }
-  Serial.printf("Время forEach QuickShiftArray: \t%lu мс\n", millis() - startTime);
-  ESP.wdtFeed();
+// ____________________Заполнение__________________________
+  Serial.println("\nЗаполнение forLoop:\n");
+  
+  Serial.printf("quickShiftArray: \t%u мс\n", forLoop(quickShiftArray));
+  Serial.printf("std::array: \t\t%u мс\n", forLoop(stdArr));
+  Serial.printf("std::vector: \t\t%u мс\n", forLoop(vector));
+  Serial.printf("classicArr: \t\t%u мс\n", forLoop(classicArr));
 
+ 
+  Serial.println("\nЗаполнение range-based for loop:\n");
 
- // ____________________Заполнение QuickShiftArray через итераторы________________________
-  c = ITERATIONS / 2;
-  startTime = millis();
-  while (c--)
-  {
-      for (auto it =  quickShiftArray.begin(); it != quickShiftArray.end(); ++it){
-        *it = 0;
-      }
-  }
-  Serial.printf("Время iterator Q.ShiftArray: \t%lu мс\n", (millis() - startTime)*2);
-  ESP.wdtFeed();
-
-}
-
-
+  Serial.printf("quickShiftArray: \t%u мс\n", rangeBasedForLoop(quickShiftArray));
+  Serial.printf("std::array: \t\t%u мс\n", rangeBasedForLoop(stdArr));
+  Serial.printf("std::vector: \t\t%u мс\n", rangeBasedForLoop(vector));
+  Serial.printf("classicArr: \t\t%u мс\n", rangeBasedForLoop(classicArr));
+  
+  Serial.println("\nЗаполнение for loop через итераторы:\n");
+  
+  Serial.printf("quickShiftArray: \t%u мс\n", iteratorForLoop(quickShiftArray));
+  Serial.printf("std::array: \t\t%u мс\n", iteratorForLoop(stdArr));
+  Serial.printf("std::vector: \t\t%u мс\n", iteratorForLoop(vector));
+ }
 void loop() {  ESP.wdtFeed(); }
